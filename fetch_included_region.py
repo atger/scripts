87d44c297f2976b264230038d7e0fe29ea61f71c
anand pratap singh <anandatgc@gmail.com>
2018-09-26 09:47:08 +0530

    compare chromosome

--- fetch_included_region.py
+++ fetch_included_region.py
@@ -22,7 +22,7 @@ with open(sys.argv[2]) as f:
 for i in df:
     gene = []
     for j in ds:
-        if i[1]>=j[1] and i[1]<=j[2]:
+        if i[0] == j[0] and i[1]>=j[1] and i[1]<=j[2]:
             if j[3] not in gene:
                 gene.append(j[3])
     result = [a for b in [i,[','.join(gene)]] for a in b]
